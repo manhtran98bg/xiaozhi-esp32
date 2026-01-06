31cadcb8cff8a4dcaf9d303c13fc3356e090594b
Trần Mạnh <manhtran98bg@gmail.com>
2026-01-06 23:00:43 +0700

    commit

---
+++ main/boards/esp32s3-oled-custom-board/esp32s3-custom-board.cc
@@ -0,0 +1,156 @@
+#include "wifi_board.h"
+#include "codecs/es8311_audio_codec.h"
+#include "display/lcd_display.h"
+#include "display/oled_display.h"
+#include "application.h"
+#include "button.h"
+#include "config.h"
+#include "led/gpio_led.h"
+#include "led/single_led.h"
+#include "assets/lang_config.h"
+#include <wifi_station.h>
+#include <esp_log.h>
+#include <esp_efuse_table.h>
+#include <driver/i2c_master.h>
+
+#include <esp_lcd_panel_io.h>
+#include <esp_lcd_panel_ops.h>
+#include <esp_lcd_gc9a01.h>
+#include "system_reset.h"
+#include "driver/gpio.h"
+#include "driver/spi_master.h"
+#include <esp_timer.h>
+#include "i2c_device.h"
+#include <esp_lcd_panel_vendor.h>
+#include <driver/spi_common.h>
+#include "power_save_timer.h"
+#include <esp_sleep.h>
+#include <driver/rtc_io.h>
+#include "freertos/FreeRTOS.h"
+#include "freertos/task.h"
+#include "lcd_gc9a01.h"
+#define TAG "CustomC3Board"
+
+// LV_FONT_DECLARE(font_puhui_16_4);
+// LV_FONT_DECLARE(font_awesome_16_4);
+
+LV_FONT_DECLARE(font_puhui_basic_14_1);
+LV_FONT_DECLARE(font_awesome_14_1);
+
+
+
+class CustomC3OledBoard : public WifiBoard {
+private:
+    i2c_master_bus_handle_t i2c_bus_;
+    esp_lcd_panel_io_handle_t panel_io_ = nullptr;
+    esp_lcd_panel_handle_t panel_ = nullptr;
+    Display* display_ = nullptr;
+    AudioCodec *audio_codec_ = nullptr;
+    Button boot_button_;
+
+    void InitializeDisplayI2c() {
+        i2c_master_bus_config_t i2c_bus_cfg = {
+            .i2c_port = I2C_NUM_0,
+            .sda_io_num = AUDIO_CODEC_I2C_SDA_PIN,
+            .scl_io_num = AUDIO_CODEC_I2C_SCL_PIN,
+            .clk_source = I2C_CLK_SRC_DEFAULT,
+            .glitch_ignore_cnt = 7,
+            .intr_priority = 0,
+            .trans_queue_depth = 0,
+            .flags = {
+                .enable_internal_pullup = 1,
+            },
+        };
+        ESP_ERROR_CHECK(i2c_new_master_bus(&i2c_bus_cfg, &i2c_bus_));
+        if (i2c_master_probe(i2c_bus_, ES8311_CODEC_DEFAULT_ADDR >> 1, 1000) != ESP_OK) {
+            ESP_LOGE(TAG, "Failed probe Codec ES8311, check hardware first.");
+            while (true) {
+                vTaskDelay(1000 / portTICK_PERIOD_MS);
+            }
+            
+        }
+        ESP_LOGI(TAG, "I2C bus initialized, found codec ES8311 device");
+        audio_codec_ = new Es8311AudioCodec(i2c_bus_, I2C_BUS, AUDIO_INPUT_SAMPLE_RATE, AUDIO_OUTPUT_SAMPLE_RATE,
+            AUDIO_I2S_GPIO_MCLK, AUDIO_I2S_GPIO_BCLK, AUDIO_I2S_GPIO_WS, AUDIO_I2S_GPIO_DOUT, AUDIO_I2S_GPIO_DIN,
+            AUDIO_CODEC_PA_PIN, AUDIO_CODEC_ES8311_ADDR);
+    }
+    void InitializeSsd1306Display() {
+        // SSD1306 config
+        esp_lcd_panel_io_i2c_config_t io_config = {
+            .dev_addr = 0x3C,
+            .on_color_trans_done = nullptr,
+            .user_ctx = nullptr,
+            .control_phase_bytes = 1,
+            .dc_bit_offset = 6,
+            .lcd_cmd_bits = 8,
+            .lcd_param_bits = 8,
+            .flags = {
+                .dc_low_on_data = 0,
+                .disable_control_phase = 0,
+            },
+            .scl_speed_hz = 400 * 1000,
+        };
+
+        ESP_ERROR_CHECK(esp_lcd_new_panel_io_i2c_v2(i2c_bus_, &io_config, &panel_io_));
+
+        ESP_LOGI(TAG, "Install SSD1306 driver");
+        esp_lcd_panel_dev_config_t panel_config = {};
+        panel_config.reset_gpio_num = -1;
+        panel_config.bits_per_pixel = 1;
+
+        esp_lcd_panel_ssd1306_config_t ssd1306_config = {
+            .height = static_cast<uint8_t>(DISPLAY_OLED_HEIGHT),
+        };
+        panel_config.vendor_config = &ssd1306_config;
+
+        ESP_ERROR_CHECK(esp_lcd_new_panel_ssd1306(panel_io_, &panel_config, &panel_));
+        ESP_LOGI(TAG, "SSD1306 driver installed");
+
+        // Reset the display
+        ESP_ERROR_CHECK(esp_lcd_panel_reset(panel_));
+        if (esp_lcd_panel_init(panel_) != ESP_OK) {
+            ESP_LOGE(TAG, "Failed to initialize display");
+            display_ = new NoDisplay();
+            return;
+        }
+        ESP_ERROR_CHECK(esp_lcd_panel_invert_color(panel_, false));
+
+        // Set the display to on
+        ESP_LOGI(TAG, "Turning display on");
+        ESP_ERROR_CHECK(esp_lcd_panel_disp_on_off(panel_, true));
+
+        display_ = new OledDisplay(panel_io_, panel_, DISPLAY_OLED_WIDTH, DISPLAY_OLED_HEIGHT, false, false);
+    }
+    void InitializeButtons() {
+        boot_button_.OnClick([this]() {
+            auto& app = Application::GetInstance();
+            if (app.GetDeviceState() == kDeviceStateStarting) {
+                EnterWifiConfigMode();
+                return;
+            }
+            ESP_LOGI(TAG, "Press");
+            app.ToggleChatState();
+        });
+    }
+public:
+    CustomC3OledBoard() : boot_button_(BOOT_BUTTON_GPIO)
+    {
+        InitializeDisplayI2c();
+        InitializeSsd1306Display();
+        InitializeButtons();
+    }
+
+    virtual Led* GetLed() override {
+        static SingleLed led(BUILTIN_LED_GPIO);
+        return &led;
+    }
+    virtual Display* GetDisplay() override {
+        return display_;
+    }
+    virtual AudioCodec* GetAudioCodec() override {
+        return audio_codec_;
+    }
+};
+
+// DECLARE_BOARD(CustomC3Board);
+DECLARE_BOARD(CustomC3OledBoard);
